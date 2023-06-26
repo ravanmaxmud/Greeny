d0d326f09525d31fd04a8441aff25f7034f8823d
Ali <aliga@code.edu.az>
2023-06-26 19:00:28 +0400

    Blog Category has been complated

---
+++ GrennyWebApplication/Areas/Admin/ViewModels/BlogCategory/AddViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace GrennyWebApplication.Areas.Admin.ViewModels.BlogCategory
+{
+    public class AddViewModel
+    {
+        public string Title { get; set; }
+
+
+        public AddViewModel()
+        {
+
+        }
+
+        public AddViewModel(string title)
+        {
+            Title = title;
+
+        }
+    }
+}
---
+++ GrennyWebApplication/Areas/Admin/ViewModels/BlogCategory/UpdateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GrennyWebApplication.Areas.Admin.ViewModels.BlogCategory
+{
+    public class UpdateViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
