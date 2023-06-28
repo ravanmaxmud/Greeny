29aa3213a6d2ded12d7ea8af63477affdba375a2
Ali <aliga@code.edu.az>
2023-06-28 16:26:19 +0400

    sorry

--- GrennyWebApplication/Areas/Admin/Controllers/UserController.cs
+++ GrennyWebApplication/Areas/Admin/Controllers/UserController.cs
@@ -19,8 +19,8 @@ namespace GrennyWebApplication.Areas.Admin.Controllers
 {
     [Area("admin")]
     [Route("admin/User")]
-    //[Authorize(Roles = "admin")]
-   
+    [Authorize(Roles = "admin")]
+
     public class UserController : Controller
     {
 
